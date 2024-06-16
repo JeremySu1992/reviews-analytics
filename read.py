40bc5b3c20232b02765bfda8e4bcf63a064155bb
Jeremy Su <withlove5313@gmail.com>
2024-06-16 14:05:11 +0800

    Add len

--- read.py
+++ read.py
@@ -4,7 +4,14 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line.strip())
 		count += 1
-		if count % 10000 == 0:
+		if count % 1000 == 0:
 			print(len(data))
-print(len(data))
-print(data[0])
+print('檔案讀取完', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+
+print('檔案讀取完', sum_len, '總字數')
+
+print('留言平均長度為', sum_len / len(data))
